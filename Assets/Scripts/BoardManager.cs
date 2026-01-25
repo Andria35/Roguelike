dc57b70550069c6371695d240eb7f5c6ced1bdd2
Andria Inasaridze <andria.inas@gmail.com>
2026-01-25 13:41:05 +0100

    Add GameManager script

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,12 +4,12 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
-
     public class CellData
     {
         public bool Passable;
     }
 
+    private CellData[,] m_BoardData;
     private Tilemap m_Tilemap;
     private Grid m_Grid;
 
@@ -17,25 +17,8 @@ public class BoardManager : MonoBehaviour
     public int Height;
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
-    public PlayerController Player;
 
-
-    private CellData[,] m_BoardData;
-
-    public CellData GetCellData(Vector2Int cellIndex)
-{
-   if (cellIndex.x < 0 || cellIndex.x >= Width
-       || cellIndex.y < 0 || cellIndex.y >= Height)
-   {
-       return null;
-   }
-
-   return m_BoardData[cellIndex.x, cellIndex.y];
-}
-
-
-    // Start is called before the first frame update
-    void Start()
+    public void Init()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
         m_Grid = GetComponentInChildren<Grid>();
@@ -63,12 +46,21 @@ public class BoardManager : MonoBehaviour
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
-
-        Player.Spawn(this, new Vector2Int(1, 1));
     }
 
     public Vector3 CellToWorld(Vector2Int cellIndex)
     {
         return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
+
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= Width
+            || cellIndex.y < 0 || cellIndex.y >= Height)
+        {
+            return null;
+        }
+
+        return m_BoardData[cellIndex.x, cellIndex.y];
     }
 }
